efa696f2a3241cfd1fdf5ed8254367c18c592069
nunocorverag <gnuno2003@gmail.com>
2023-10-12 11:44:53 -0600

    Insertion_Sort Algorithm

---
+++ Insertion_Sort.py
@@ -0,0 +1,24 @@
+#Insertion Sort
+
+#The array is split into a sorted and unsorted part. 
+#The values from the unsorted part are picked and placed at the correct position in the sorted part
+
+#Iterates over the arre and compares the kurrent element to its predecessor,
+#if the key element is smaller than its predecessor, it compares it to the elements before.
+
+def Insertion_Sort(nums):
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+
+        # Compare key with each element on the left of it until an element smaller than it is found
+        while j >= 0 and key < nums[j]:
+            nums[j + 1] = nums[j]
+            j = j - 1
+
+        nums[j + 1] = key
+    return(nums)
+def main():
+    print(Insertion_Sort([5,1,7,4,9,12,2,3]))
+
+main()
